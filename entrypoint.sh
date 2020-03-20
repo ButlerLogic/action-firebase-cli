be73d3c73f05f83651f337e394ed8fe50a3b5057
Corey Butler <corey@coreybutler.com>
2020-03-20 14:49:59 -0500

    Fixed syntax issue with entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-if [[ -v FIREBASE_TOKEN ]]; then
+if [ -v FIREBASE_TOKEN ]; then
     echo "Cannot find FIREBASE_TOKEN. Was it configured in the repository secrets?"
     exit 1
 fi
 
-if [[ -v FIREBASE_PROJECT ]]; then
+if [ -v FIREBASE_PROJECT ]; then
     echo "FIREBASE_PROJECT is missing"
     exit 1
 fi
 
-if [[ -v COMMAND ]]; then
+if [ -v COMMAND ]; then
   echo "COMMAND is missing. Nothing to run."
   exit 1
 fi
 
-if [[ -v PRERUN ]]; then
+if [ -v PRERUN ]; then
   ${PRERUN}
 fi
 
