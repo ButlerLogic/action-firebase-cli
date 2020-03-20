267ca6e5ca589dc7da6bd49eae2328864a60476c
Corey Butler <corey@coreybutler.com>
2020-03-20 14:56:34 -0500

    Fix bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-if [ -v FIREBASE_TOKEN ]; then
+if [ -z "${FIREBASE_TOKEN}" ]; then
     echo "Cannot find FIREBASE_TOKEN. Was it configured in the repository secrets?"
     exit 1
 fi
 
-if [ -v FIREBASE_PROJECT ]; then
+if [ -z "${FIREBASE_PROJECT}" ]; then
     echo "FIREBASE_PROJECT is missing"
     exit 1
 fi
 
-if [ -v COMMAND ]; then
+if [ -z "${FIREBASE_COMMAND}" ]; then
   echo "COMMAND is missing. Nothing to run."
   exit 1
 fi
 
-if [ -v PRERUN ]; then
+if [ -n "${PRERUN}" ]; then
   ${PRERUN}
 fi
 
